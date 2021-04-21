66e4639a72078ef84913459ad70e004b40e9e590
John M. Knutson Sr <john.m.knutson@gmail.com>
2021-04-21 17:31:48 -0500

    updates!

--- cmd/one-wire-temp.go
+++ cmd/one-wire-temp.go
@@ -29,6 +29,8 @@ Options:`)
 	println(`
 Environment Variables:
   DEVICES_DIR - directory path containing one wire device directories
+  HOSTNAME - hostname to interpolate in mq topic
+  MQ_BROKER - mq broker, ex: "tcp://localhost:1833"
   POLL_INTERVAL - interval (in seconds) at which to poll for temperature
 `)
 	println(`For more information, see https://github.com/jknutson/one-wire-temp-go`)
@@ -64,7 +66,6 @@ func main() {
 	setupCloseHandler()
 
 	// mqtt setup
-	// TODO: get this from env
 	mqHostname, ok := os.LookupEnv("HOSTNAME")
 	if !ok {
 		mqHostname = "raspberrypi" // default if HOSTNAME env var is not set
@@ -118,10 +119,11 @@ func main() {
 			}
 
 			temperatureFahrenheit := fmt.Sprintf("%f", (temperatureCelcius*1.8)+32)
+      mqTopic := fmt.Sprintf("%s-%s/temperature", mqTopicBase, device)
 			if verbose {
-				log.Printf("%s-%s/temperature %s", mqTopicBase, device, temperatureFahrenheit)
+				log.Printf("%s %s", mqTopic, temperatureFahrenheit)
 			}
-			token := c.Publish(fmt.Sprintf("%s-%s/temperature", mqTopicBase, device), 0, false, temperatureFahrenheit)
+			token := c.Publish(mqTopic, 0, false, temperatureFahrenheit)
 			token.Wait()
 			if token.Error() != nil {
 				log.Printf("mq publish error: %s\n", token.Error())
